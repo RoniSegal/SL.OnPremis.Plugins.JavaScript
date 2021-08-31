6ccb4ed69314364d3008e1e3cf0c081fc7cf8730
Roni Segal <roni@sealights.io>
2021-08-31 16:39:11 +0300

    Change in common

--- packages/sealights-plugins-common/lib/cli-args-parser.ts
+++ packages/sealights-plugins-common/lib/cli-args-parser.ts
@@ -1,7 +1,7 @@
 import {ITestRunnerArgs} from "slnodejs";
 
 const minimist = require('minimist');
-
+// Comment
 export function getSLArgs(): ITestRunnerArgs {
     const args = minimist(process.argv.slice(2));
     const slKeys = Object.keys(args).filter(key => key.indexOf('sl-') === 0);
